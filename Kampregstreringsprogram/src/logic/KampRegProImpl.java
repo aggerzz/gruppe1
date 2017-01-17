ed5ba66d779ce0ca7316cb82f932162853df5944
Jeppe <Jeppe@Darknesskiller.ceuherning.int>
2017-01-17 12:41:13 +0100

    Har ændret KampRegProImpl

--- Kampregstreringsprogram/src/logic/KampRegProImpl.java
+++ Kampregstreringsprogram/src/logic/KampRegProImpl.java
@@ -11,7 +11,7 @@ public class KampRegProImpl implements KampRegProInterface {
 	private KampData kam = new KampData();
 
 	@Override
-	public Kamp opretKamp(Hold hjemmehold, Hold udehold) {
+	public Kamp opretKamp(String hjemmehold, String udehold) {
 		Kamp kamp = new Kamp();
 		kamp.setHjemmehold(hjemmehold);
 		kamp.setUdehold(udehold);
