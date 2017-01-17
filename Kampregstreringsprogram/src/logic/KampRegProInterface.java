9710380ef835a4457c1ac0ebe546a50855a2a908
Jeppe <Jeppe@Darknesskiller.ceuherning.int>
2017-01-17 12:39:08 +0100

    Ændret KampRegProInterface

--- Kampregstreringsprogram/src/logic/KampRegProInterface.java
+++ Kampregstreringsprogram/src/logic/KampRegProInterface.java
@@ -2,12 +2,11 @@ package logic;
 
 import java.util.List;
 
-import domain.Hold;
 import domain.Kamp;
 import domain.Liga;
 
 public interface KampRegProInterface {
-	public Kamp opretKamp(Hold hjemmehold, Hold udehold);
+	public Kamp opretKamp(String hjemmehold, String udehold);
 	public void opdaterKamp(Kamp kamp);
 	public List<Kamp> visKampe(String holdnavn);
 	public void opdaterLiga(Liga liga);
