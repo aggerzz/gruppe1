9764015631cc6fdeaccf0a5f09df8a8b89217629
Maibritt <Maibritt@MonstersPC.ceuherning.int>
2017-01-18 09:17:03 +0100

    pas liga data

--- Kampregstreringsprogram/src/data/Ligadata.java
+++ Kampregstreringsprogram/src/data/Ligadata.java
@@ -1,5 +1,19 @@
 package data;
 
+import domain.Liga;
+
 public class Ligadata {
+	public Liga visliga(String liganavn){
+		try(DataAccess access = new DataAccess()){
+			try{
+				visliga(access, liganavn);
+				access.commit();
+			}catch(Exception e){
+				access.rollback();
+				throw e; 
+			}
+		}
+		return ;
+	}
 
 }
