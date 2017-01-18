797242daca1faf749624e4ed5bd281e216a49862
Jeppe <Jeppe@Darknesskiller.ceuherning.int>
2017-01-18 09:09:35 +0100

    Tilføjet funktion

--- Kampregstreringsprogram/src/data/KampData.java
+++ Kampregstreringsprogram/src/data/KampData.java
@@ -1,9 +1,27 @@
 package data;
 
+import java.sql.PreparedStatement;
+
 import domain.Kamp;
 
 public class KampData {
 	public boolean gemKamp(Kamp kamp){
 		return false;
+	}
+	public Kamp opretKamp(String hjemmehold, String udehold){
+		Kamp kamp = new Kamp();
+		try(DataAccess access = new DataAccess()){
+			try{
+				opretKamp(access, hjemmehold, udehold);
+				access.commit();
+			}catch(Exception e){
+				access.rollback();
+				throw e; 
+			}
+		}
+		return kamp;
+	}
+	public Kamp opretKamp(DataAccess access, String hjemmehold, String udehold){
+		try(PreparedStatement statement = access.getConnection().prepareStatement(""))
 	}
 }
