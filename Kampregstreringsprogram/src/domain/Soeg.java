ed06c4b6c2027566338ed8fead80d43a1596f4ad
Maibritt <Maibritt@MonstersPC.ceuherning.int>
2017-01-25 09:44:48 +0100

    Kom til at slette klassen så den er genoprettet

--- Kampregstreringsprogram/src/domain/Soeg.java
+++ Kampregstreringsprogram/src/domain/Soeg.java
@@ -4,20 +4,21 @@ public class Soeg {
 
 	private int holdid;
 	private String holdnavn;
-	
+
 	public int getHoldid() {
 		return holdid;
 	}
+
 	public void setHoldid(int holdid) {
 		this.holdid = holdid;
 	}
+
 	public String getHoldnavn() {
 		return holdnavn;
 	}
+
 	public void setHoldnavn(String holdnavn) {
 		this.holdnavn = holdnavn;
 	}
-
-	
 
 }
